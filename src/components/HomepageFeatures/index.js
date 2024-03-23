30da0eddc8ea19f6eeb1e28d638b11a273ffc269
Stephan Bauer <stephan.bauer@catena-x.net>
2024-03-23 12:39:14 +0100

    landing page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,30 +4,39 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Get Support',
+    title: 'Get Support / How to contribute',
     Svg: require('@site/static/img/support-svgrepo-com.svg').default,
     description: (
       <>
-          We have an 1h <a href="https://eclipse-tractusx.github.io/community/open-meetings">Office Hour</a> Meeting every Friday. Feel free to join and ask your questions directly there.<br/><br/>
-          You can also join us on Catena-X <b>MS Teams</b> channel <b><a target="_blank" href="https://teams.microsoft.com/l/channel/19%3a9a3c4a05a3514d07b973c13e7b468709%40thread.tacv2/CX%2520-%2520CoP%2520DevSecOps?groupId=17b1a2dc-67fb-4a49-a2ed-dd1344321439&tenantId=1ad22c6d-2f08-4f05-a0ba-e17f6ce88380">Communities of Practices -> CX - CoP DevSecOps</a></b>.
+          Fededback/Information/Reuests via <b>GitHub</b> <a href="https://github.com/catenax-eV/catenax-ev.github.io/discussions">discussions</a> <br/><br/>
+          Want to support/contribute ...
       </>
     ),
   },
   {
-    title: 'Get Started',
+    title: 'Standards',
     Svg: require('@site/static/img/rocket-svgrepo-com.svg').default,
     description: (
       <>
-          We ask you to make yourself familiar with our Documentation first by starting with our <a href="/docs/operating-model/overview">Getting Started</a> document.
+          Here you will find all the information about the <a href="/docs/standards/overview">Standards</a>.
       </>
     ),
   },
   {
-    title: 'Request resources',
-    Svg: require('@site/static/img/tools-and-utensils-hardware-svgrepo-com.svg').default,
+    title: 'Operating Model',
+    Svg: require('@site/static/img/rocket-svgrepo-com.svg').default,
     description: (
       <>
-        Feel free to browse our catalog of <a href="/docs/resources">Resources you can request</a> directly from us.
+          Here you will find all the information about the <a href="/docs/operating-model/overview">Operating Model</a>.
+      </>
+    ),
+  },
+  {
+    title: 'Working Model',
+    Svg: require('@site/static/img/rocket-svgrepo-com.svg').default,
+    description: (
+      <>
+          Here you will find all the information about the <a href="/docs/working-model/overview">Working Model</a>.
       </>
     ),
   },
