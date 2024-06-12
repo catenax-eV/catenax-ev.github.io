adf0502775ba9a9fc94e9ee94388e384aeda8888
Stephan Bauer <stephan.bauer@catena-x.net>
2024-06-12 22:04:49 +0200

    again fix typo

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -37,7 +37,7 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Fededback',
+    title: 'Feedback',
     Svg: require('@site/static/img/support-icon.svg').default,
     description: (
       <>
