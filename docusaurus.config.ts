80056a2e02ced2e9ee713f2ce55a71a5ef1627b9
Johann Schütz <johann@schuetz.pro>
2024-08-20 18:19:25 +0200

    Docusaurus Update 3.4.0 -> 3.5.2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'catenax-eV', // Usually your GitHub org/user name.
-  projectName: 'catenax-ev.github.io.new', // Usually your repo name.
+  projectName: 'catenax-ev.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
