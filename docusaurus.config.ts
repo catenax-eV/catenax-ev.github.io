3b361a65329d6160a8353917af251669c0fc195b
Stephan Bauer <stephan.bauer@catena-x.net>
2026-01-20 12:49:19 +0100

    Update sidebar configuration and change markdown link handling to warn

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -102,7 +102,7 @@ const config: Config = {
   markdown: {
     mermaid: true,
     hooks: {
-      onBrokenMarkdownLinks: 'throw',
+      onBrokenMarkdownLinks: 'warn',
       onBrokenMarkdownImages: 'throw',
     },
   },
